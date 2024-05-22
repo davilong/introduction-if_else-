621b75ab9c7bda5c4582f5a43ed4ca41c4086e3f
Davi Gabriel Da Silva Marques <00001121646712sp@aluno.educacao.sp.gov.br>
2024-05-22 17:29:13 -0300

    firt commit

---
+++ introduction(if_else) ex2.py
@@ -0,0 +1,5 @@
+nota_final = float(input("Digite a nota final"))
+if nota_final >=7:
+    print("Parabens! Você foi aprovado. ")
+else:
+    print("infelizmente, você não foi aprovado. Estude mais. ")
